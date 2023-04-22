09e2c9f3ceca1cd9b7ff9aa13a250d38742217e0
JustGetAClass <mohamedsalat62@gmail.com>
2023-04-22 12:28:42 +0300

    added assets

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,8 +1,17 @@
 import React from "react";
+import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
 import "./navbar.css";
 
 const NavBar = () => {
-  return <div>NavBar</div>;
+  return (
+    <div className="gpt4__navbar">
+      <div className="gpt4__navbar-links">
+        <div className="gpt3__navbar-links_logo">
+          <img src={logo} alt="logo" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default NavBar;
